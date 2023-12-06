8a6d4ebeaf6a94b5598c1ef194d0633583dc2c06
Gabriel Galdino <92198024+Gabriel-Gald1n0@users.noreply.github.com>
2023-12-05 22:57:14 -0300

    add documentação das questões da lista

---
+++ Documentacao_questoes/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../questao14_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;questao14.c'],['../questao18_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;questao18.c'],['../questao19_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;questao19.c'],['../questao20_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;questao20.c']]]
+];
---
+++ Documentacao_questoes/html/search/all_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['questao14_2ec_0',['questao14.c',['../questao14_8c.html',1,'']]],
+  ['questao18_2ec_1',['questao18.c',['../questao18_8c.html',1,'']]],
+  ['questao19_2ec_2',['questao19.c',['../questao19_8c.html',1,'']]],
+  ['questao20_2ec_3',['questao20.c',['../questao20_8c.html',1,'']]]
+];
